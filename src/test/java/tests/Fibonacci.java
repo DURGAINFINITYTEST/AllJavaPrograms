a5d447721d84da37b638653d9a649a0378eb874a
durga5237 <katakatladurgaprasad@gmail.com>
2024-02-24 20:49:45 +0530

    All java programs

--- src/test/java/tests/Fibonacci.java
+++ src/test/java/tests/Fibonacci.java
@@ -10,10 +10,7 @@ public class Fibonacci {
 		fib[0] = 0;
 		fib[1] = 1;
 
-		/*
-		 * fib[2] = 1; fib[3] = 2; fib[4] = 3; fib[5] = 5; fib[6] = 8; fib[7] = 13;
-		 * fib[8] = 21; fib[9] = 34;
-		 */
+		
 
 		System.out.print(fib[0] + " " + fib[1] + " ");
 
@@ -26,8 +23,6 @@ public class Fibonacci {
 
 		System.out.println(Math.random());
 
-		Random rn = new Random();
-		System.out.println(rn.nextInt(198, 200));;;;;;
 	}
 
 }
---
+++ src/test/resources/fisrt.feature
@@ -0,0 +1,12 @@
+
+Feature: login functionality
+
+Scenario: login with valid inputs 
+
+Given  user navigate to the login page
+When   user enter the valid gmail id  and passwword
+And    user  click to login
+Then   user should be login successfully
+  
+
+ 
